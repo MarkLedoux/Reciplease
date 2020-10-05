27aa137dda92d221ee9cb5645cb99f125aac0cef
vinceled <mark.ledoux@live.fr>
2020-10-05 13:19:25 +0200

    creation of the reciplease project

---
+++ Reciplease/Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Mark LEDOUX on 05/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
